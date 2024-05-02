353e688334f50f67cb8c015069ae2f659d4c7050
YehorVahin <davyjohns322@gmail.com>
2024-05-02 19:31:55 +0300

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,18 @@
 'use strict';
 
-function slugify(title){
-    return title.toLowerCase().split(' ').join('-')
-}
-console.log(slugify("Arrays for begginers"));
-console.log(slugify("English for developer"));
-console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+function isEnoughCapacity(products, containerSize) {
+    const totalProducts = Object.values(products).reduce((acc, curr) => acc + curr, 0);
+    return totalProducts <= containerSize;
+  }
+  console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  );
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  );
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+  );
+  console.log(
+    isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+  );
